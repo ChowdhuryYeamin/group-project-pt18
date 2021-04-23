f7371506ada078f5b452f1bb5e26e66d495c2dea
RYan <gargy.krysinski@gmail.com>
2021-04-22 18:05:25 -0700

    Added some getters and setters and finished other stuff I started yesterday.

--- dataprinter.h
+++ dataprinter.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <vector>
+#include <string>
 
 #include "metadatafinder.h"
 
@@ -12,8 +13,43 @@ class DataPrinter: public MetaDataFinder{
 	public:
 	void WriteFile{
 		int i;
+		ofstream fout("Metadata.csv");
 		for(i = 0; i < data.size(); i++){
-			
+			fout << "File Type: " << data[i].getftype() << endl;
+			fout << "Modification Date: " << data[i].getmoddate << endl;
+			fout << "Location: " << data[i].getlocation << endl;
+			fout << "Method: " << data[i].getmethod << endl;
+			fout << "CRC: " << data[i].getcrc << endl;
+			fout << "Compression: " << data[i].getcomp << endl;
+			fout << "Index: " << data[i].getindex << endl;
+			fout << "Size: " << data[i].getsize << endl;	
+		}
+		fout.close()
+	}
+	std::string getftype(){
+		return file_type;
+	}
+	std::string getmoddate(){
+		return modification_date;
+	}
+	std::string getlocation(){
+		return location;
+	}
+	std::string getmethod(){
+		return method;
+	}
+	std::string getcrc(){
+		return CRC;
+	}
+	int getcomp(){
+		return compressed;
+	}
+	int getindex(){
+		return index;
+	}
+	int getsize(){
+		return size;
+	}			
 };
 
 #endif
--- metadatafinder.h
+++ metadatafinder.h
@@ -13,10 +13,46 @@ class MetaDataFinder{
 	int size, compressed, index;
 	vector<MetaDataFinder> data;
 	public:
-	MetaDataFinder(std::string file_type, std::string modification_date, std::string location, std::string method, std::string CRC, int size, int compressed, int index);
+	MetaDataFinder(std::string file_type, std::string modification_date, std::string location, std::string method, std::string CRC, int size, int compressed, int index){
+		setfiletype(file_type);
+		setmoddate(modification_date);
+		setlocation(location);
+		setmethod(method);
+		setcrc(CRC);
+		setsize(size);
+		setcomp(compressed);
+		setindex(index);
+	}
 	void scanMetaData();
 	void scanTechSpecs();
-	void addToVector();
+	void addToVector(){
+		MetaDataFinder *newpoint = new MetaDataFinder(file_type, modification_date, location, method, CRC, size, compressed, index);
+		data.push_back(*newpoint);
+	}
+	void setfiletype(std::string new_file_type){
+		file_type = new_file_type;
+	}
+	void setmoddate(std::string new_modification_date){
+		modification_date = new_modification_date;
+	}
+	void setlocation(std::string new_location){
+		location = new_location;
+	}
+	void setmethod(std::string new_method){
+		method = new_method;
+	}
+	void setcrc(std::string new_CRC){
+		CRC = new_CRC;
+	}
+	void setsize(int new_size){
+		size = new_size;
+	}
+	void setcomp(int new_compressed){
+		compressed = new_compressed;
+	}
+	void setindex(int new_index){
+		index = new_index;
+	}
 };
 	
 #endif
