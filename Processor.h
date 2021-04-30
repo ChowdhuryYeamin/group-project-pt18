5e662cc0277c040692f70511942d65b2c47c0b42
rkrysinski1023 <70532714+rkrysinski1023@users.noreply.github.com>
2021-04-30 15:29:54 -0700

    Change Processor.h to better align with Yeamin's code.

--- Processor.h
+++ Processor.h
@@ -1,10 +1,11 @@
-#define PROCESSOR_H
 #ifndef PROCESSOR_H
+#define PROCESSOR_H
 
 using namespace std;
 
 class Processor{
-	virtual void Processor(unsigned char* soundvalues, int numvalues) = 0;
+	void Processor8(unsigned char* soundvalues, int numvalues) = 0;
+	void Processor16(unsigned char* soundvalues, int numvalues) = 0;
 };
 
 #endif
