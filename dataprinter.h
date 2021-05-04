01cfea19c4073df33a4a00028a7baa46bca89e17
rkrysinski1023 <70532714+rkrysinski1023@users.noreply.github.com>
2021-05-04 14:55:21 -0700

    Added doxygen comments to dataprinter

--- dataprinter.h
+++ dataprinter.h
@@ -5,19 +5,29 @@
 #include <vector>
 #include <string>
 
-#define LENGTH 500
-
 #include "Wav.h"
-#include "audiofile.h"
 
 using namespace std;
 
 class DataPrinter: public Wav{
-         public:
-     DataPrinter();
-     ~DataPrinter();
+     	public:
+	/**
+ * @brief This is a dataprinter class, it inherits from the Wav class
+ */
+     	DataPrinter();
+	/**
+ * @brief This is a constructor for the dataprinter class;
+ */
 
-          void WriteFile(vector<Audio*> list);
+	~DataPrinter();
+	/**
+ * @brief This is a destructor for the dataprinter class.
+ */
+
+    	void WriteFile(vector<Audio*> list);
+	/**
+ * @brief Writes off key technical information and metadata for all files using a far loop. Takes in the vector list of all the audio files as an argument and loops through them accordingly. 
+ */
 };
 
 #endif
